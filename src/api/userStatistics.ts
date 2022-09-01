491fdd2dd107ad4c562ddd7ff9af2b35ebe1c249
Alexander Marunich <myrakes4korn@gmail.com>
2022-09-01 21:42:16 +0300

    refactor: replace function to appropriate place

--- src/api/userStatistics.ts
+++ src/api/userStatistics.ts
@@ -1,5 +1,5 @@
 import { API_BASE_LINK } from '../constants/constants';
-import { OptionalWord, UserStatsResponse, WordStatsResponse } from '../constants/types';
+import { UserStatsResponse } from '../constants/types';
 import state from '../state/state';
 
 export async function getUserStatistics(): Promise<UserStatsResponse | void> {
@@ -35,43 +35,7 @@ export async function setUserStatistics(optional: UserStatsResponse): Promise<Us
             body: JSON.stringify(optional),
         });
         const data = await response.json();
-        console.log(response);
         return data;
-    }
-    return undefined;
-}
-
-export async function getWordStatistics(): Promise<WordStatsResponse | void> {
-    try {
-        const { userId } = state.user;
-        if (userId) {
-            const response = await fetch(`${API_BASE_LINK}/users/${userId}/words`, {
-                method: 'GET',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },
-            });
-            return response.json();
-        }
-    } catch {
-        console.log('new entry of word statistics');
-    }
-    return undefined;
-}
-
-export async function setWordStatistics(wordId: string, optional: OptionalWord): Promise<WordStatsResponse | void> {
-    const { userId } = state.user;
-    if (userId) {
-        const response = await fetch(`${API_BASE_LINK}/users/${userId}/words/${wordId}`, {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(optional),
-        });
-        return response.json();
     }
     return undefined;
 }
--- src/api/words.ts
+++ src/api/words.ts
@@ -1,7 +1,43 @@
-import { WORDS } from '../constants/constants';
-import { Word } from '../constants/types';
+import { API_BASE_LINK, WORDS } from '../constants/constants';
+import { OptionalWord, Word, WordStatsResponse } from '../constants/types';
+import state from '../state/state';
 
 export default async function getWords(group: number, page: number): Promise<Word[]> {
     const response = await fetch(`${WORDS}?group=${group}&page=${page}`);
     return response.json();
+}
+export async function getWordStatistics(): Promise<WordStatsResponse | void> {
+    try {
+        const { userId, token } = state.user;
+        if (userId) {
+            const response = await fetch(`${API_BASE_LINK}/users/${userId}/words`, {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    Accept: 'application/json',
+                },
+            });
+            return response.json();
+        }
+    } catch {
+        console.log('new entry of word statistics');
+    }
+    return undefined;
+}
+
+export async function setWordStatistics(wordId: string, optional: OptionalWord): Promise<WordStatsResponse | void> {
+    const { userId, token } = state.user;
+    if (userId) {
+        const response = await fetch(`${API_BASE_LINK}/users/${userId}/words/${wordId}`, {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${token}`,
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(optional),
+        });
+        return response.json();
+    }
+    return undefined;
 }
