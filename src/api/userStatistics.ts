02d7a620dc094f858145737ebcf923ef795d2a13
Olga Simankova <o.kotlyarova@gmail.com>
2022-09-02 10:38:05 +0300

    fix: remove redundant comments, switch local server to heroku

--- src/api/userStatistics.ts
+++ src/api/userStatistics.ts
@@ -40,34 +40,3 @@ export async function setUserStatistics(optional: UserStatsResponse): Promise<Us
     }
     return undefined;
 }
-
-// export async function getWordStatistics(): Promise<WordStatsResponse | void> {
-//     const { userId } = state.user;
-//     if (userId) {
-//         const response = await fetch(`${API_BASE_LINK}/users/${userId}/words`, {
-//             method: 'GET',
-//             headers: {
-//                 Accept: 'application/json',
-//                 'Content-Type': 'application/json',
-//             },
-//         });
-//         return response.json();
-//     }
-//     return undefined;
-// }
-
-// export async function setWordStatistics(wordId: string, optional: OptionalWord): Promise<WordStatsResponse | void> {
-//     const { userId } = state.user;
-//     if (userId) {
-//         const response = await fetch(`${API_BASE_LINK}/users/${userId}/words/${wordId}`, {
-//             method: 'POST',
-//             headers: {
-//                 Accept: 'application/json',
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify(optional),
-//         });
-//         return response.json();
-//     }
-//     return undefined;
-// }
--- src/logic/textbook/vocabulary.ts
+++ src/logic/textbook/vocabulary.ts
@@ -1,29 +1,6 @@
 import { setUserWordStats } from '../../api/words';
 import { WordStatus } from '../../constants/types';
 import state from '../../state/state';
-import { checkTokenExpiration } from '../main/authentication';
-
-async function changeVocabularyViewOnAuth() {
-    // const textbookBtn = document.querySelector('#textbook') as HTMLElement;
-    // const vocabularyBtn = document.querySelector('#vocabulary') as HTMLElement;
-    const categoryContainer = document.querySelector('.word_categories_container') as HTMLElement;
-
-    const isTokenOK = checkTokenExpiration();
-    if (isTokenOK) {
-        categoryContainer.classList.toggle('active', true);
-        // const words = await getUserAggregatedWords();
-    } else {
-        // Получаем новый токен
-        // Если не получилось добыть новый, показываем надпись "Только авторизованным пользователям..."
-        // Если успеем, можно показывать другое сообщение, типа "Ваша сессия истекла. Требуется повторная авторизация", если пользователь ранее был авторизован
-    }
-
-    if (state.user.isAuthenticated) {
-        categoryContainer.classList.toggle('active', true);
-    } else {
-        categoryContainer.classList.toggle('active', false);
-    }
-}
 
 export const listenTextbookTitleView = () => {
     const headingContainer = document.querySelector('.heading_section') as HTMLElement;
