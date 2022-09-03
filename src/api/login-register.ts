f36fe5870581c7cd74a2b02493714138474af33c
nester-dev <nester-dev@gmail.com>
2022-09-03 21:55:57 +0300

    fix: an error during registration

--- src/api/login-register.ts
+++ src/api/login-register.ts
@@ -2,17 +2,28 @@ import { API_BASE_LINK } from '../constants/constants';
 import { User, UserResponse } from '../constants/types';
 import state from '../state/state';
 
-const createUser = async (user: User): Promise<UserResponse> => {
-    return (
-        await fetch(`${API_BASE_LINK}/users`, {
+const createUser = async (user: User): Promise<UserResponse | undefined> => {
+    let response;
+    try {
+        response = await fetch(`${API_BASE_LINK}/users`, {
             method: 'POST',
             headers: {
                 Accept: 'application/json',
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(user),
-        })
-    ).json();
+        });
+        if (!response.ok) {
+            const message = document.querySelector('.user-exist') as HTMLElement;
+            message.style.visibility = 'visible';
+            response = undefined;
+        } else {
+            response = response.json();
+        }
+    } catch (error) {
+        console.error(error);
+    }
+    return response;
 };
 
 const loginUser = async (user: User): Promise<UserResponse> => {
